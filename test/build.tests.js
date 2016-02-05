a10e1dd7311d5fa20839b0be59f4fee1e9ef82c6
jpage <jpage@godaddy.com>
2016-02-05 11:46:14 -0800

    Convert test back to ES5

--- test/build.tests.js
+++ test/build.tests.js
@@ -4,14 +4,14 @@ const childProcess = require('child_process');
 const path = require('path');
 const package = require('../package.json');
 
-describe('The module build', () => {
+describe('The module build', function () {
   it('creates an importable module', function(done) {
     this.timeout(60000);
     
     const rootDir = path.resolve(__dirname, '..');
     childProcess
       .exec('npm run build:module', { cwd: rootDir })
-      .on('exit', exitCode => {
+      .on('exit', function (exitCode) {
         expect(exitCode).to.equal(0);
         
         const ctor = require(path.resolve(rootDir, package.main));
