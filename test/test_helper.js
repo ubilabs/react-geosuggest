ef6c1cf7d5204bc61d4e7484fad1abf2f8ada0be
Narendra Shetty <mailnarendra15@gmail.com>
2016-03-10 09:16:52 +0530

    setup basic testing infrastructure

---
+++ test/test_helper.js
@@ -0,0 +1,13 @@
+const jsdom = require('jsdom');
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
