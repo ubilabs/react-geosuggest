a1010b9a054cb15396cbf98b5707f8286d76159e
Blake Yoder <yoderblake@gmail.com>
2017-07-19 13:52:31 -0400

    Call input blur when a suggest is selected

--- src/Geosuggest.jsx
+++ src/Geosuggest.jsx
@@ -360,6 +360,10 @@ class Geosuggest extends React.Component {
     }
 
     this.geocodeSuggest(suggest);
+    // on `enter`, input is not blurred so we need
+    // to manually call blur() so that suggestions
+    // list will repopulate on focus()
+    this.blur();
   };
 
   /**
