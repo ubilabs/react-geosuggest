74e19673fdb09ceb345ec74e5d0bba9092fe9250
Robert Katzki <katzki@ubilabs.net>
2020-05-08 14:17:07 +0200

    refactor(example): use function component (#461)

--- example/src/app.tsx
+++ example/src/app.tsx
@@ -5,69 +5,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Geosuggest from '../../src/Geosuggest';
 
-class App extends React.Component {
+const App = (): JSX.Element => {
   /**
    * When the input receives focus
    */
-  onFocus(): void {
-    console.log('onFocus');
-  }
+  const onFocus = (): void => console.log('onFocus');
 
   /**
    * When the input loses focus
    */
-  onBlur(value?: string): void {
-    console.log('onBlur', value);
-  }
+  const onBlur = (value?: string): void => console.log('onBlur', value);
 
   /**
    * When the input got changed
    */
-  onChange(value: string): void {
+  const onChange = (value: string): void =>
     console.log(`input changes to : ${value}`);
-  }
 
   /**
    * When a suggest got selected
    */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  onSuggestSelect(suggest: any): void {
-    console.log(suggest);
-  }
+  const onSuggestSelect = (suggest: any): void => console.log(suggest);
 
   /**
    * When there are no suggest results
    */
-  onSuggestNoResults(userInput: string): void {
+  const onSuggestNoResults = (userInput: string): void =>
     console.log(`onSuggestNoResults for : ${userInput}`);
-  }
 
-  /**
-   * Render the example app
-   * @return {Function} React render function
-   */
-  render(): JSX.Element {
-    const fixtures = [
-      {label: 'New York', location: {lat: 40.7033127, lng: -73.979681}},
-      {label: 'Rio', location: {lat: -22.066452, lng: -42.9232368}},
-      {label: 'Tokyo', location: {lat: 35.673343, lng: 139.710388}}
-    ];
+  const fixtures = [
+    {label: 'New York', location: {lat: 40.7033127, lng: -73.979681}},
+    {label: 'Rio', location: {lat: -22.066452, lng: -42.9232368}},
+    {label: 'Tokyo', location: {lat: 35.673343, lng: 139.710388}}
+  ];
 
-    return (
-      <div>
-        <Geosuggest
-          fixtures={fixtures}
-          onFocus={this.onFocus}
-          onBlur={this.onBlur}
-          onChange={this.onChange}
-          onSuggestSelect={this.onSuggestSelect}
-          onSuggestNoResults={this.onSuggestNoResults}
-          location={new google.maps.LatLng(53.558572, 9.9278215)}
-          radius="20"
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <Geosuggest
+      fixtures={fixtures}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onChange={onChange}
+      onSuggestSelect={onSuggestSelect}
+      onSuggestNoResults={onSuggestNoResults}
+      location={new google.maps.LatLng(53.558572, 9.9278215)}
+      radius="20"
+    />
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
