a791826297b1d447a6d1b15690e84d024dfbba3a
Robert Katzki <katzki@ubilabs.net>
2020-06-19 15:34:25 +0200

    fix: change comments… 🙈

--- src/Geosuggest.tsx
+++ src/Geosuggest.tsx
@@ -483,8 +483,7 @@ export default class extends React.Component<IProps, IState> {
   // eslint-disable-next-line complexity
   selectSuggest(suggestToSelect: ISuggest | null): void {
     let suggest: ISuggest = suggestToSelect || {
-      // isFixture: true,
-      isFixture: false,
+      isFixture: true,
       label: this.state.userInput,
       placeId: this.state.userInput
     };
