91671377067ed81889b4e29be5cb4a9e58e5f14e
joaogarin <joao.garin@gmail.com>
2021-10-18 12:39:17 +0100

    fix(Geosuggest): fix accessibility

--- src/input.tsx
+++ src/input.tsx
@@ -40,7 +40,7 @@ export default class Input extends React.PureComponent<IProps, unknown> {
    */
   static defaultProps: IProps = {
     activeSuggest: null,
-    autoComplete: 'nope',
+    autoComplete: 'off',
     className: '',
     isSuggestsHidden: true,
     listId: '',
